9effd30e330e6315237b5ca70f40ff5d2b2c6978
Mehul Lakhanpal <318097@gmail.com>
2024-01-29 15:17:00 +0530

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Login from "./Login";
-import Home from "./Home";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
+      <Login />
       <Home />
     </div>
   );
